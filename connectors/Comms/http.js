3b3b9fc3741aa80c0e33bc169e48ab4003149a36
Yeinso Blanco <yeinsojbm@gmail.com>
2024-08-26 15:48:18 -0500

    mailer

---
+++ connectors/Comms/http.js
@@ -0,0 +1,29 @@
+const Rest = require('../../drivers/rest')
+
+
+module.exports = class commsConnector extends Rest {
+  constructor(host, app, headers, templates, logger) {
+    super(host, '/v1', 'Comms', app, headers, logger);
+    this.templates = templates;
+  }
+
+  sendMail(to, template, data) {
+    const body = {
+      destination: to,
+      template_id: this.templates[template] || '',
+      data   
+    }
+    this.send('/mail', 'post', body)
+      .then(({ status, data }) => {
+        const { request_id, message  } = data;
+        this.logger.info(
+          `Mail sent to ${body.destination} in ${request_id} with status ${status}: ${message}
+        `)
+      })
+      .catch((error) => {
+        this.logger.error(`Mail send error: ${error}`)
+      });
+  }
+
+  
+};
